d5d66594aa24b80d436e73301b233a354ad7903a
Bartholome <bartholome.lebruno@gmail.com>
2020-03-30 12:55:39 +0200

    correction

--- src/__tests__/Pokemon.spec.ts
+++ src/__tests__/Pokemon.spec.ts
@@ -4,11 +4,11 @@ describe('Test ../Pokemon.ts', () => {
     test('should equals', () => {
         let pokemon = new Pokemon("Pikachu",45);
         expect(pokemon.name).toBe("Pikachu");
-    })
-    test('should attaque first', () => {
+    });
+    test('should first pokemon attaque first', () => {
         let pokemon = new Pokemon("Pikachu",45);
         let pokemon2 = new Pokemon("ratata",30);
 
         expect(pokemon.attaquefirst(pokemon2)).toBe(true);
-    })
+    });
 });
