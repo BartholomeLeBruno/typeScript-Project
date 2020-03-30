a5f9ba76d1b08d43af0e23043d6a57eb9cb9d54d
Bartholome <bartholome.lebruno@gmail.com>
2020-03-30 11:09:41 +0200

    pokemon file

---
+++ src/Pokemon.ts
@@ -0,0 +1,7 @@
+export class Pokemon{
+    name: any;
+    constructor(name: any) {
+        this.name = name;
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {Pokemon} from './Pokemon';
+
+let pokemon = new Pokemon("pikachu");
+
+console.log(pokemon.name);
+
